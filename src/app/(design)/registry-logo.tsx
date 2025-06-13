1e2bfaae992e1cd49ba1480b7e1157a78b77ae2f
Jared Thompson <jared.thompson@vercel.com>
2025-06-13 09:51:32 -0700

    r we up?

--- src/app/(design)/registry-logo.tsx
+++ src/app/(design)/registry-logo.tsx
@@ -1,10 +1,10 @@
-import { Squirrel } from "lucide-react";
+import Image from "next/image";
 
 export function RegistryLogo() {
   return (
     <>
       <div className="flex-shrink-0 rounded-md bg-primary p-1">
-        <Squirrel className="size-5 text-secondary" />
+        <Image src="/upwork.svg" alt="Upwork Logo" width={32} height={32} />
       </div>
       <span className="font-semibold">Registry</span>
     </>
